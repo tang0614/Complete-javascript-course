385c06052642640fc4ae35503ad883926ca0f40a
tang0614 <your_email@example.com>
2019-03-13 14:32:34 -0400

    js

---
+++ 2-JS-basics/starter/operators.js
@@ -0,0 +1,33 @@
+// Basic operators
+
+var year, month, day
+
+year = 2019
+month = 3
+day = 13
+
+console.log(year + 12)
+console.log(day/3)
+
+//logical operators
+console.log(year > month)
+console.log(typeof month)
+
+//operator precendence 
+var num = year - month >= day
+console.log(num)
+
+
+//multiple assignment 
+x = y = (3+5) * 4 -6 
+console.log(x,y)
+
+//more operation
+x *= 2
+console.log(x)
+
+x += 2
+console.log(x)
+
+x++
+console.log(x)
---
+++ 2-JS-basics/starter/variable.js
@@ -0,0 +1,41 @@
+console.log('hello world!')
+
+
+/*
+This file is about variables and datatypes, variable mutations
+
+1.number all have point numbers, for decimasl adn integers
+2.string is sequence of character
+3.boolean
+4.undefined is a variable does not have a value
+5.Null is also non-existence
+
+***** data types are automatically assigned to variables *****
+
+*/
+
+var firstname = 'anna'
+var age = 28
+
+console.log(firstname + '' + age)
+
+
+var job, isMarried
+job = 'teacher'
+isMarried = false
+
+console.log(firstname + 'is a ' + age + ' year old ' + job)
+
+
+// small pop up window on web
+
+job = 'driver'
+isMarried = false
+
+//alert(firstname + ' is a ' + age + ' year old ' + job) 
+
+
+//ask question
+var lastname = prompt('what is his last name?')
+console.log(firstname + ' ' +lastname)
+
