183ace1221191889f3bf6ddd07271777cc1a6395
tang0614 <your_email@example.com>
2019-03-18 18:16:46 -0400

    js

---
+++ 2-JS-basics/starter/loops and iteration.js
@@ -0,0 +1,36 @@
+var john = ['john','smith',1990,'designer','false','blue']
+
+for(var i = 0; i<john.length; i++){
+	console.log(john[i])
+}
+
+//while loop
+var i = 0
+
+while(i<john.length){
+	console.log(john[i])
+	i++
+}
+
+//use continue and break 
+
+for(var i = 0; i < john.length; i++){
+	if(typeof john[i] !== 'string'){
+		continue
+	}
+	console.log(john[i])
+}
+
+
+for(var i = 0; i < 5; i++){
+	if(typeof john[i] !== 'string'){
+		break
+	}
+	console.log('exist')
+}
+
+
+//looping backward
+for(var i = john.length - 1; i>=0; i--){
+	console.log(john[i])
+}
